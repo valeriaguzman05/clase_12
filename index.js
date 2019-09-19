49129d18f4229a8cd3542fd479e089116649825e
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-09-18 20:38:35 -0500

    Cooler message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer(function(req, res) {
         res.writeHead(200, { 'Content-Type' : 'text/plain'})
-        res.write('okay, you can sleep now')
+        res.write('You can sleep now')
         res.end()
     })
 
