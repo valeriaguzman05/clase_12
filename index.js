16c05d5b9757a8fceb44a03316d890b65b4385e0
Valeria Guzman <valeria.guzman0509@gmail.com>
2019-09-21 22:49:57 -0500

    second option for http local server added to index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 
 const server = http.createServer(function(req, res) {
         res.writeHead(200, { 'Content-Type' : 'text/plain'})
@@ -6,4 +6,17 @@ const server = http.createServer(function(req, res) {
         res.end()
     })
 
-    server.listen(9000)
+    server.listen(9000)*/
+
+
+const http = require("http")
+const port = 8000
+
+function responseHandler (req, res) {
+    res.write("Hello Ducky!")
+    res.end()
+}
+
+const server = http.createServer(responseHandler)
+server.listen(port)
+
